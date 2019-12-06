3f2ff87d5a5ee2e4da0e860dc5e89c480186d0ab
Mohammad Jawarneh <jawarndo@gmail.com>
2019-12-06 20:56:40 +0000

    Added boilerplate for JavaFx

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static int width = 1000;
+    private static int height = 800;
+
+    /*
+    * Launch window, set initial menu scene.
+    * @params stage: set the stage object.
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Expense System");
+        //stage.setScene(someScene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
