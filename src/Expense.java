6f5d15b0b241cb3c2b4129b3f9a01cde6b8866b6
Mohammad Jawarneh <jawarndo@gmail.com>
2019-12-06 22:31:09 +0000

    Created basic Expense schema

--- src/Expense.java
+++ src/Expense.java
@@ -1,5 +1,24 @@
 public class Expense {
 
-    Expense() {}
+    private double amount;
+    private String category;
+    private String desc;
 
+    Expense(double amount, String category, String desc) {
+        this.amount = amount;
+        this.category = category;
+        this.desc = desc;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 }
