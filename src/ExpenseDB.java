e6966393dfb374247cca80cdc5fa6a80c37edbb6
Mohammad Jawarneh <jawarndo@gmail.com>
2019-12-06 23:14:03 +0000

    Added file parser and db file

--- src/ExpenseDB.java
+++ src/ExpenseDB.java
@@ -1,2 +1,46 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ExpenseDB {
+
+    private ArrayList<Expense> expenseArrayList;
+    private final String PATH = "/Users/jawarneh/Documents/Projects/Expense-System/storage/storage.txt";
+
+    /*
+    * Constructor only read files and populates array list.
+     */
+    ExpenseDB() {
+        readDB();
+    }
+
+    /*
+    * Method reads the contents of the database then parses it
+      such that all records are stored into the expenseArrayList.
+    *
+    * @return instance of ArrayList containing instances of Expense.
+     */
+    private ArrayList<Expense> readDB() {
+        String contents = "";
+        ArrayList<Expense> parsedExpenses = new ArrayList<>();
+
+        // Wrap scanner in case of failure to site the file.
+        try(Scanner scanner = new Scanner(new FileReader(this.PATH))) {
+            // Appends file contents to variable.
+            while (scanner.hasNextLine()) {
+                contents += scanner.nextLine() + "\n";
+            }
+        }
+        catch (FileNotFoundException e) {
+            // Print errors for developer for now.
+            e.printStackTrace();
+        }
+
+        return parsedExpenses;
+    }
+
+    private void writeDB() {
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
